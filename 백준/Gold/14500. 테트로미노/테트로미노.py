f6c68c94ee1459908c98194cfe3f45daa0e85a67
박준일 <116266030+june21a@users.noreply.github.com>
2026-03-30 22:29:02 +0900

    [Gold IV] Title: 테트로미노, Time: 4896 ms, Memory: 40148 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.py
+++ 백준/Gold/14500. 테트로미노/테트로미노.py
@@ -6,62 +6,45 @@ N, M = map(int, input().split())
 score_map = []
 for i in range(N):
     score_map.append(list(map(int, input().split())))
+visited = [[False for i in range(M)] for j in range(N)]
 
 def check_idx(y, x):
-    return y >= 0 and y < N and x >= 0 and x < M
+    return y >= 0 and y < N and x >= 0 and x < M and not visited[y][x]
 
-def reverse_xy(block):
-    for i in range(4):
-        block[i] = block[i][::-1]
-    return block
 
-def reverse_x_sign(block):
-    for i in range(4):
-        block[i][1] *= -1 
-    return block
-
-def reverse_y_sign(block):
-    for i in range(4):
-        block[i][0] *= -1 
-    return block
-
-def get_block_by_number(block, number):
-    if number & 1:
-        block = reverse_xy(block)
-    
-    if number & 2:
-        block = reverse_x_sign(block)
-    
-    if number & 4:
-        block = reverse_y_sign(block)
-    return block
-
-# block making
-blocks = [
-    [[0, 0], [0, 1], [0, 2], [0, 3]],
-    [[0, 0], [1, 0], [2, 0], [3, 0]],
-    [[0, 0], [0, 1], [1, 0], [1, 1]],
-    [[0, 0], [1, 0], [2, 0], [2, 1]],
-    [[0, 0], [1, 0], [1, 1], [2, 1]],
-    [[0, 0], [0, 1], [1, 1], [0, 2]]
-]
-additional_block = []
-for block in blocks[3:]:
-    for i in range(1, 8):
-        additional_block.append(get_block_by_number(copy.deepcopy(block), i))
-blocks.extend(additional_block)
-
+# without T block
+dx = [0, 0, 1, -1]
+dy = [1, -1, 0, 0]
 ans = 0
+
+def dfs(y, x, S, depth):
+    if depth == 4:
+        global ans
+        ans = max(ans, S+score_map[y][x])
+        return
+    
+    visited[y][x] = True
+    for i in range(4):
+        next_y, next_x = y+dy[i], x+dx[i]
+        if check_idx(next_y, next_x):
+            dfs(next_y, next_x, S+score_map[y][x], depth+1)
+
+    visited[y][x] = False
+    
 for i in range(N):
     for j in range(M):
-        for block in blocks:
-            S = 0
-            poses = [[i+dy, j+dx] for dy, dx in block]
-            
-            for (y, x) in poses:
-                if not check_idx(y, x):
-                    break
-                S += score_map[y][x]
+        dfs(i, j, 0, 1)
 
-            ans = max(ans, S)
-print(ans)
+# for T block
+for i in range(N):
+    for j in range(M):
+        lst = []
+        
+        for k in range(4):
+            next_y, next_x = i+dy[k], j+dx[k]
+            if check_idx(next_y, next_x):
+                lst.append(score_map[next_y][next_x])
+            else:
+                lst.append(0)
+        ans = max(ans, score_map[i][j] + sum(lst) - min(lst))
+print(ans)   
